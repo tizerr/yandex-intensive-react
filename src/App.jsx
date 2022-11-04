c27b980385831958a4ace657b4dc32587ccd3260
ilya159368 <72142468+ilya159368@users.noreply.github.com>
2022-11-04 18:05:43 +0300

    Changed App.jsx. Set CatalogPage as initial page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import {Layout} from "./components/Layout";
-// import {CatalogPage} from "./pages/CatalogPage";
-import {BookPage} from "./pages/BookPage";
+import {CatalogPage} from "./pages/CatalogPage";
+// import {BookPage} from "./pages/BookPage";
 import {categories} from "./constants/mock";
 
 function App() {
-    const testBook = categories[0].books[0]
+    // const testBook = categories[0].books[0]  // for BookPage
     return (
       <Layout>
-          {/*<CatalogPage categories={categories}/>*/}
-          <BookPage book={testBook}></BookPage>
+          <CatalogPage categories={categories}/>
+          {/*<BookPage book={testBook}></BookPage>*/}
       </Layout>
     );
 }
